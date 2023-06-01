34833332db6fc8375573766fde33c680fad846b7
Eder Santo <eder0782@gmail.com>
2023-06-01 17:23:44 +0000

    animations and array images

--- src/pages/teste/index.tsx
+++ src/pages/teste/index.tsx
@@ -1,30 +1,27 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import {
-  Title,
   FlexContainer,
   Container,
   Wrapper,
-  GridContainer,
+  ImageContainer,
 } from "./teste.style";
 import Image from "next/image";
 
 export default function Teste() {
-  const blocos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-  return (
-    <Container>
-      <FlexContainer>
-        {blocos.map((value, index) => {
-          return <Imagem key={index}></Imagem>;
-        })}
-      </FlexContainer>
-    </Container>
-  );
-}
+  const [listImages, setListImages] = useState<string[]>([]);
+  const [carregado, setCarregado] = useState(false);
 
-const Imagem = () => {
-  const [hidde, setHidde] = React.useState(true);
-  const [src, setSrc] = React.useState("");
   const imagens: string[] = [
+    "angularjs.png",
+    "css3.png",
+    "vue-js.png",
+    "stack-overflow.png",
+    "raspberry-pi.png",
+    "python.png",
+    "nodejs.png",
+    "mongodb.png",
+    "html5.png",
+    "github.png",
     "angularjs.png",
     "css3.png",
     "vue-js.png",
@@ -37,6 +34,68 @@ const Imagem = () => {
     "github.png",
   ];
 
+  const listImagesGenerate = useCallback(() => {
+    let indice = imagens.length;
+
+    while (indice) {
+      // atenção para o pós-incremento indice--
+      const indiceAleatorio = Math.floor(Math.random() * indice--);
+      [imagens[indice], imagens[indiceAleatorio]] = [
+        imagens[indiceAleatorio],
+        imagens[indice],
+      ];
+    }
+    setTimeout(() => {
+      setCarregado(true);
+      // console.log(imagens, ":imagens");
+    }, 2000);
+    // setCarregado(true);
+    // setTimeout(() => {
+    //   // setListImages(imagens);
+    //   console.log(listImages, ":listImages");
+    // }, 4000);
+  }, [setListImages]);
+
+  React.useEffect(() => {
+    listImagesGenerate();
+  }, []);
+
+  React.useEffect(() => {
+    setListImages(imagens);
+    setTimeout(() => console.log(listImages, ":listaImagens"), 2000);
+  }, [carregado]);
+
+  const blocos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+  return (
+    <Container>
+      <FlexContainer>
+        {imagens.map((value, index) => {
+          return <Imagem image={value} key={index}></Imagem>;
+        })}
+      </FlexContainer>
+    </Container>
+  );
+}
+
+const Imagem = (props: any) => {
+  const [hidde, setHidde] = React.useState(true);
+  const [src, setSrc] = React.useState("");
+  const { image } = props;
+
+  const imagens: string[] = [
+    "angularjs.png",
+    "css3.png",
+    "vue-js.png",
+    "stack-overflow.png",
+    "raspberry-pi.png",
+    "python.png",
+    "nodejs.png",
+    "mongodb.png",
+    "html5.png",
+    "github.png",
+  ];
+  // console.log(image, ":image");
+
   const handleImage = React.useCallback(() => {
     const index = Math.floor(Math.random() * 10);
     setSrc(`/images/${imagens[index]}`);
@@ -44,14 +103,17 @@ const Imagem = () => {
   React.useEffect(() => handleImage(), []);
   return (
     <Wrapper onClick={() => setHidde(!hidde)} ativo={hidde}>
-      <Image
-        src={src}
-        alt="Vercel Logo"
-        width={80}
-        height={80}
-        hidden={hidde}
-        priority
-      />
+      <ImageContainer ativo={hidde}>
+        {image && (
+          <Image
+            src={`/images/${image}`}
+            alt="Vercel Logo"
+            width={80}
+            height={80}
+            priority
+          />
+        )}
+      </ImageContainer>
     </Wrapper>
   );
 };
--- src/pages/teste/teste.style.ts
+++ src/pages/teste/teste.style.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const Title = styled.h1`
   font-size: 1.5em;
@@ -6,7 +6,6 @@ export const Title = styled.h1`
   color: #bf4f74;
 `;
 
-// Create a Wrapper component that'll render a <section> tag with some styles
 export const Wrapper = styled.section<{ ativo: boolean }>`
   height: 6rem;
   width: 6rem;
@@ -14,16 +13,23 @@ export const Wrapper = styled.section<{ ativo: boolean }>`
   font-size: 0;
   color: #ffffff;
   border-radius: 8px;
-
   cursor: pointer;
   transition: 0.5s;
   display: flex;
   justify-content: center;
   align-items: center;
   box-shadow: 5px 2px 20px 0 rgb(46 61 73 / 50%);
-  @media (max-width: 464px){
+  @media (max-width: 464px) {
     margin-bottom: 1.5rem;
   }
+`;
+
+export const ImageContainer = styled.div<{ ativo: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: ${(props) => (props.ativo ? 0 : 1)};
+  transition: 0.5s;
 `;
 export const Container = styled.div`
   margin: 0;
@@ -44,25 +50,23 @@ export const FlexContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   margin: 0 0 3em;
-
-  
 `;
-export const GridContainer = styled.div`
-  margin: 0;
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-auto-rows: 8rem;
-  grid-column-gap: 1rem;
-  grid-row-gap: 1rem;
-  background: white;
+// export const GridContainer = styled.div`
+//   margin: 0;
+//   display: grid;
+//   grid-template-columns: repeat(4, 1fr);
+//   grid-auto-rows: 8rem;
+//   grid-column-gap: 1rem;
+//   grid-row-gap: 1rem;
+//   background: white;
 
-  @media (max-width: 1024px) {
-    grid-template-columns: repeat(3, 1fr);
-  }
-  @media (max-width: 820px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-  @media (max-width: 560px) {
-    grid-template-columns: repeat(1, 1fr);
-  }
-`;
+//   @media (max-width: 1024px) {
+//     grid-template-columns: repeat(3, 1fr);
+//   }
+//   @media (max-width: 820px) {
+//     grid-template-columns: repeat(2, 1fr);
+//   }
+//   @media (max-width: 560px) {
+//     grid-template-columns: repeat(1, 1fr);
+//   }
+// `;
